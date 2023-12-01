86f099e0190f185b48e0bc38804ffcc7127f8f69
Song Kim Hian <songk@DESKTOP-M3I2KTO>
2023-12-01 16:22:54 +0800

    Add project files.

---
+++ PT/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace PT;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ PT/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿namespace PT;
+
+public partial class MainPage : ContentPage
+{
+
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private void Image_SizeChanged(object sender, EventArgs e)
+    {
+
+    }
+}
+
